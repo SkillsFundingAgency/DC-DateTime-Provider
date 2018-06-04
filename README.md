31bd565e74561816b5669afe2e323f55cb97bd9e
John Youren <john.youren@gmail.com>
2018-06-04 13:01:58 +0100

    Initial check in

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DC-DateTime-Provider
+
+Provides methods for dealing with dates & times. All dates & times are to be stored in UTC; only being converted to local format at render time.
+
+```
+System.DateTime GetNowUtc();
+
+System.DateTime ConvertUtcToUk(System.DateTime utcDateTime);
+
+System.DateTime ConvertOpaToLocalDateTime(string opaDateTime);
+```
---
+++ src/ESFA.DC.DateTime.Provider/DateTimeProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using ESFA.DC.DateTime.Provider.Interface;
+
+namespace ESFA.DC.DateTime.Provider
+{
+    public sealed class DateTimeProvider : IDateTimeProvider
+    {
+        private static readonly IFormatProvider culture = new CultureInfo("en-GB", true);
+
+        public System.DateTime GetNowUtc()
+        {
+            return System.DateTime.UtcNow;
+        }
+
+        public System.DateTime ConvertUtcToUk(System.DateTime utcDateTime)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+        }
+
+        public System.DateTime ConvertOpaToLocalDateTime(string opaDateTime)
+        {
+            return Convert.ToDateTime(opaDateTime, culture);
+        }
+    }
+}
